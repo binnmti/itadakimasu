7db23912e61d66a5f04df1b1ff87d432dfb0169c
bin matsui <BinMatsui@hotmail.com>
2022-01-08 01:45:40 +0900

    WEBとAPIを作る

--- itadakimasu/Program.cs
+++ itadakimasu/Program.cs
@@ -1,25 +1,30 @@
-using itadakimasu.Models;
+using FoodNameChecker.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ItadakimasuContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("ItadakimasuContext")));
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
-//builder.Services.AddAuthorization(options => { options.FallbackPolicy = options.DefaultPolicy; });
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 app.UseHttpsRedirection();
-//app.UseAuthentication();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
+app.MapRazorPages();
 app.Run();
