a7c868e8016e6e8561cfeceaac30acea9f657fc5
bin matsui <BinMatsui@hotmail.com>
2022-01-09 22:49:43 +0900

    フォルダ整理

---
+++ Models/Food.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Food
+    {
+        public long Id { get; set; }
+        [MaxLength(500)]
+        public string Name { get; set; } = "";
+    }
+}
