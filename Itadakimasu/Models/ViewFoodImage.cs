7f208e003c4a27eff2b18fdddd3ea9ea674b4367
bin matsui <BinMatsui@hotmail.com>
2022-02-06 01:13:09 +0900

    とりあえず一覧表示

--- Itadakimasu/Models/ViewFoodImage.cs
+++ Itadakimasu/Models/ViewFoodImage.cs
@@ -2,14 +2,25 @@
 
 namespace Itadakimasu.Models
 {
-    public record ViewFoodImage(bool Checked, string Name, int X, int Y, long Size, string BlobUrl, string BaseUrl);
+    public record ViewFoodImage(bool Checked, string Name, string XY, string Size, string BlobUrl, string BaseUrl);
 
     public static class VewFoodImageConvert
     {
         private static readonly string BlobUrl = "https://itadakimasu.blob.core.windows.net/foodimage";
 
         public static IEnumerable<ViewFoodImage> ToViewFoodImages(this IEnumerable<FoodImage> foodImages)
-            => foodImages.Select(x => new ViewFoodImage(true, x.BlobName, x.BlobWidth, x.BlobHeight, x.BlobSize, x.ToBlobUrl(), x.BaseUrl));
+            => foodImages.Select(x => new ViewFoodImage(true, x.BlobName, x.ToXY(), x.ToSize(), x.ToBlobUrl(), x.BaseUrl));
+
+        private static string ToXY(this FoodImage foodImage)
+            => $"{foodImage.BlobWidth}X{foodImage.BlobHeight}";
+
+        private static string ToSize(this FoodImage foodImage)
+        {
+            if (foodImage.BlobSize < 1024) return $"{foodImage.BlobSize}B";
+            else if (foodImage.BlobSize < 1024 * 1024) return $"{foodImage.BlobSize / 1024}KB";
+            else if (foodImage.BlobSize < 1024 * 1024 * 1024) return $"{foodImage.BlobSize / 1024 / 1024}MB";
+            else return $"{foodImage.BlobSize / 1024 / 1024 / 1024}GB";
+        }
 
         private static string ToBlobUrl(this FoodImage foodImage) => $"{BlobUrl}/{foodImage.FoodName}/{foodImage.BlobSName}";
     }
