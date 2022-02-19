611d495c13881fd6a83a5844edb22588931c3d94
bin matsui <BinMatsui@hotmail.com>
2022-02-19 19:57:43 +0900

    checkを付ける

--- Itadakimasu/wwwroot/js/site.js
+++ Itadakimasu/wwwroot/js/site.js
@@ -7,12 +7,20 @@ function reloadSelect(id) {
     location.href = url.href;
 }
 // 「全て選択」チェックで全てにチェック付く
-function allChecked() {
-    var check = document.getElementById("check-all").checked;
+function allChecked(element) {
+    var checked = element.checked;
     var checkbox = document.getElementsByName("checkbox");
     for (var i = 0; i < checkbox.length; i++) {
-        checkbox[i].checked = check;
+        checkbox[i].checked = checked;
     }
+}
+function checkSync(element) {
+    var checked = element.checked;
+    var eles = document.getElementsByClassName(element.className);
+    Array.prototype.filter.call(eles, function (element) {
+        var input = element;
+        input.checked = checked;
+    });
 }
 function changeImage(name, nextName) {
     document.getElementById(name).checked = false;
--- Itadakimasu/wwwroot/js/site.ts
+++ Itadakimasu/wwwroot/js/site.ts
@@ -8,12 +8,21 @@
 }
 
 // 「全て選択」チェックで全てにチェック付く
-function allChecked() {
-    const check = (document.getElementById("check-all") as HTMLInputElement).checked;
+function allChecked(element: HTMLElement) {
+    const checked = (element as HTMLInputElement).checked;
     const checkbox = document.getElementsByName("checkbox");
     for (let i = 0; i < checkbox.length; i++) {
-        (checkbox[i] as HTMLInputElement).checked = check;
+        (checkbox[i] as HTMLInputElement).checked = checked;
     }
+}
+
+function checkSync(element: HTMLElement) {
+    const checked = (element as HTMLInputElement).checked;
+    const eles = document.getElementsByClassName(element.className);
+    Array.prototype.filter.call(eles, function (element) {
+        const input = element as HTMLInputElement;
+        input.checked = checked;
+    });
 }
 
 function changeImage(name: string, nextName: string): void {
