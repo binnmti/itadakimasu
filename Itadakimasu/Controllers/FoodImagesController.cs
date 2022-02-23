017905ee4ad02fb378654f0a6cbf419adbb1f369
bin matsui <BinMatsui@hotmail.com>
2022-02-23 23:49:35 +0900

    デバッグ以外ではサインインしてないと押せないように

--- Itadakimasu/Controllers/FoodImagesController.cs
+++ Itadakimasu/Controllers/FoodImagesController.cs
@@ -2,24 +2,22 @@
 using Itadakimasu.Data;
 using Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Itadakimasu.Controllers
 {
-    internal static class FoodImageExtention
-    {
-        internal static IQueryable<FoodImage> StateNumber(this IQueryable<FoodImage> foodImages, int stateNumber)
-            => stateNumber == 0 ? foodImages : foodImages.Where(x => x.StatusNumber == stateNumber);
-    }
-
     [Route("api/[controller]")]
     [ApiController]
     public class FoodImagesController : ControllerBase
     {
         private readonly ItadakimasuContext _context;
+        private SignInManager<IdentityUser> SignInManager { get; }
 
-        public FoodImagesController(ItadakimasuContext context)
+        public FoodImagesController(ItadakimasuContext context, SignInManager<IdentityUser> signInManager)
         {
             _context = context;
+            SignInManager = signInManager;
         }
 
         public record Food(string Name, int FoodImageCount, FoodImage First);
@@ -41,11 +39,15 @@ namespace Itadakimasu.Controllers
         public async Task<ActionResult<int>> FoodImageListCount(string foodName, int stateNumber = 0)
             => await _context.FoodImage.StateNumber(stateNumber).CountAsync(x => x.FoodName == foodName);
 
-
+        [Authorize]
         public record FoodImageRequest(long Id, int StateNumber);
         [HttpPost("food-image-state")]
         public async Task<ActionResult<FoodImage>> FoodImageState([FromBody] FoodImageRequest request)
         {
+#if !DEBUG
+            //TODO:これでもまだ弱い。本当は管理者のみ
+            if (!SignInManager.IsSignedIn(User)) return;
+#endif
             var hit = await _context.FoodImage.SingleOrDefaultAsync(x => x.Id == request.Id);
             if (hit == null) return Conflict();
 
@@ -54,10 +56,14 @@ namespace Itadakimasu.Controllers
             return hit;
         }
 
+        [Authorize]
         public record FoodImageAllRequest(List<long> Ids, int StateNumber);
         [HttpPost("food-image-all-state")]
         public void FoodImageAllState([FromBody] FoodImageAllRequest request)
         {
+#if !DEBUG
+            if (!SignInManager.IsSignedIn(User)) return;
+#endif
             var hits = _context.FoodImage.Where(x => request.Ids.Any(i => i == x.Id)).ToList();
             hits.ForEach(x => x.StatusNumber = request.StateNumber);
             _context.SaveChanges();
@@ -85,5 +91,11 @@ namespace Itadakimasu.Controllers
 
         private async Task<FoodImage?> FindAsync(string baseUrl)
             => await _context.FoodImage.SingleOrDefaultAsync(x => x.BaseUrl == baseUrl);
+    }
+
+    internal static class FoodImageExtention
+    {
+        internal static IQueryable<FoodImage> StateNumber(this IQueryable<FoodImage> foodImages, int stateNumber)
+            => stateNumber == 0 ? foodImages : foodImages.Where(x => x.StatusNumber == stateNumber);
     }
 }
