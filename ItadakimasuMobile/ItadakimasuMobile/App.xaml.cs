107d7d0d34fcda4634d61883bfdabe71cc89cd95
Bin Matsui <binmatsui@hotmail.com>
2022-03-17 08:39:04 +0900

    Xamarin追加

---
+++ ItadakimasuMobile/ItadakimasuMobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ItadakimasuMobile.Services;
+using ItadakimasuMobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ItadakimasuMobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
